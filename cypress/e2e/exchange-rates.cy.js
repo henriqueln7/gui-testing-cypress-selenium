8eeb55274c594380a4a4ab8cbb5cbc9c99ce9583
Thiago Moura <thiago.moura@ccc.ufcg.edu.br>
2023-05-28 12:26:37 -0300

    feat/add exchange rate script

--- cypress/e2e/exchange-rates.cy.js
+++ cypress/e2e/exchange-rates.cy.js
@@ -1,4 +1,4 @@
-describe('currencies', () => {
+describe('exchange rates', () => {
   beforeEach(() => {
     cy.visit('/admin');
     cy.get('[id="_username"]').type('sylius');
@@ -6,18 +6,20 @@ describe('currencies', () => {
     cy.get('.primary').click();
   });
   // Remove .only and implement others test cases!
-  it.only('validate that cannot create the same currency twice', () => {
-    // Click in currencies in side menu
-    cy.clickInFirst('a[href="/admin/currencies/"]');
+  it.only('create a new exchange rate between USD and EUR', () => {
+    // Click in exchange rates in side menu
+    cy.clickInFirst('a[href="/admin/exchange-rates/"]');
     // Click on create button
     cy.get('*[class^="ui labeled icon button  primary "]').click();
-    // Select Euro currency
-    cy.get('#sylius_currency_code').select('EUR');
-
+    // Select US Dollar Source currency
+    cy.get('#sylius_exchange_rate_sourceCurrency').select('USD');
+    // Type ratio to 5
+    cy.get('#sylius_exchange_rate_ratio').type('5');
     // Click on create button
     cy.get('*[class^="ui labeled icon primary button"]').scrollIntoView().click();
-    // Assert that cannot create the same currency twice
-    cy.get('body').should('contain', 'This form contains errors.').and('contain', 'Currency code must be unique.');
+
+    // Assert that exchange rate has been created
+    cy.get('body').should('contain', 'Exchange rate has been successfully created.');
   });
   it('test case 2', () => {
     // Implement your test case 2 code here
