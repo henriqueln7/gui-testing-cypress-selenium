8c0365d55627c89dc53daf378509b28df009efaa
Thiago Moura <thiago.moura@ccc.ufcg.edu.br>
2023-05-28 19:25:06 -0300

    feat/minor change

--- cypress/e2e/taxons.cy.js
+++ cypress/e2e/taxons.cy.js
@@ -14,7 +14,7 @@ describe('taxons', () => {
     // Click in edit item in dropdown
     cy.get('li:nth-child(3) > .sylius-tree__item > .sylius-tree__action > form > .ui > .menu > .item:nth-child(2)').click();
     // Edit dresses name
-    cy.get('[id="sylius_taxon_translations_en_US_name"]').clear().type(' Top');
+    cy.get('[id="sylius_taxon_translations_en_US_name"]').clear().type('Dresses Top');
     // Click on Save changes button
     cy.get('[id="sylius_save_changes_button"]').scrollIntoView().click();
 
