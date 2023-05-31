433b3c7fb704832f1be9d5cadbee987a38fe3dbc
Henrique Lopes <henriquelopees7@gmail.com>
2023-05-30 21:17:48 -0300

    tests: Testes usando o Cypress

--- cypress/e2e/products.cy.js
+++ cypress/e2e/products.cy.js
@@ -37,6 +37,7 @@ describe('products', () => {
     cy.get('#sylius_product_translations_en_US_name').type(randomString());
     cy.get('#sylius_product_translations_en_US_slug').type(randomString());
     cy.get('.primary').click();
+
     cy.get('.sylius-flash-message').should('contain', 'Product has been successfully created.');
 
   });
@@ -60,16 +61,31 @@ describe('products', () => {
     cy.get('#criteria_enabled').select('true');
     cy.get('#criteria_channel').select('Fashion Web Store');
 
-    //Click on element with class .button and href=/admin/products
-    cy.get('.button[href="/admin/products/"]').click();
+    cy.get('.button[href="/admin/products/"] > .remove').click();
 
     cy.get('#criteria_search_value').should('have.value', '');
-  })
+  });
+
+  it('should apply filters in the listing', () => {
+    cy.visit('http://localhost:8080/admin/products/');
+    cy.get('.loadable').click();
+    cy.get('#criteria_search_value').type('jeans');
+    cy.get('#criteria_enabled').select('true');
+    cy.get('#criteria_channel').select('Fashion Web Store');
+
+    //Botão de filtrar
+
+    cy.get('.sylius-filters ~ .icon.button > .icon.search').click();
+
+    cy.get('div.ui.segment.spaceless.sylius-grid-table-wrapper > table > tbody > tr').each((el) => {
+      cy.wrap(el).should('contain', 'jeans');
+    });
+  });
 
   it('should be able to edit name/description of a product', function() {
     cy.visit('http://localhost:8080/admin/products/');
 
-    cy.get('.button[href="/admin/products/20/edit"').click();
+    cy.get('.button[href="/admin/products/92/edit"').click();
 
     cy.get('#sylius_product_translations_en_US_name').type(randomString());
     cy.get('#sylius_product_translations_en_US_description').type(randomString());
@@ -79,16 +95,69 @@ describe('products', () => {
   });
 
   it('should be able to remove the image media from a product', function() {
+    cy.visit('http://localhost:8080/admin/products/');
     cy.visit('http://localhost:8080/admin/products/?page=2');
-    cy.get('.button[href="/admin/products/11/edit"').click();
-    cy.get('.item[data-tab="media"]').click();
-
-    cy.get['a[data-form-collection="delete"]'].click();
+    cy.get('.button[href="/admin/products/87/edit"').click();
+    cy.get('body > div.admin-layout.admin-layout--open > div.admin-layout__body > div.admin-layout__content > div.ui.segment > form > div.ui.stackable.grid.sylius-tabular-form > div.three.wide.column > div > a:nth-child(5)').click();
 
     cy.get('.save.icon').click();
     cy.get('.sylius-flash-message').should('contain', 'Product has been successfully updated.');
 
   });
 
-  // Implement the remaining test cases in a similar manner
+  it('should not remove products if the user click NO', function() {
+    cy.visit('http://localhost:8080/admin/products/');
+    cy.get('td').contains('000F_office_grey_jeans').click();
+    cy.get('td').contains('007M_black_elegance_jeans').click();
+    cy.get('.item:nth-child(2) .bulk-select-checkbox').click();
+    cy.get('form:nth-child(1) > .red').click();
+    cy.get('.cancel').click();
+
+    cy.get('table').contains('td', '000F_office_grey_jeans');
+    cy.get('table').contains('td', '007M_black_elegance_jeans');
+  });
+
+  it('should remove products if the user click YES', function() {
+    //CRIA PRODUTO
+    let productName = randomString();
+    cy.clickInFirst('a[href="/admin/products/"]');
+    cy.get('.primary > .text').click();
+    cy.get('[href="/admin/products/new/simple"]').click();
+    cy.get('#sylius_product_code').type(productName);
+    cy.get('#sylius_product_variant_channelPricings_FASHION_WEB_price').type('40');
+    cy.get('#sylius_product_translations_en_US_name').type(randomString());
+    cy.get('#sylius_product_translations_en_US_slug').type(randomString());
+    cy.get('.primary').click();
+
+    // FILTRA PRODUTO
+    cy.visit('http://localhost:8080/admin/products/');
+    cy.get('.loadable').click();
+    cy.get('#criteria_search_value').type(productName);
+
+    cy.get('.sylius-filters ~ .icon.button > .icon.search').click();
+
+
+
+    //REMOVE PRODUTO
+
+    cy.get('td').contains(productName).click();
+    cy.get('.item:nth-child(1) .bulk-select-checkbox').click();
+    cy.get('form:nth-child(1) > .red').click();
+    cy.get('#confirmation-button').click();
+
+
+    cy.get('.sylius-flash-message').should('contain', 'Products have been successfully deleted.');
+  })
+
+  it('should see the details of a product and add to cart', function() {
+    cy.visit('http://localhost:8080/en_US/products/990m-regular-fit-jeans');
+    cy.get('#main-image').click();
+    cy.get('.lb-close').click();
+    cy.get('#sylius_add_to_cart_cartItem_variant_jeans_size').select('M');
+    cy.get('#sylius_add_to_cart_cartItem_quantity').type('2');
+    cy.get('#sylius_add_to_cart_cartItem_quantity').click();
+    cy.get('.primary').click();
+
+    cy.get('.sylius-flash-message').should('contain', 'Item has been added to cart');
+  });
 });
